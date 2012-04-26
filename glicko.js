2605e9a0c988ee6e50b3b745e73489447604caa9
Rob Kohr <spam@robkohr.com>
2012-04-26 07:41:59 -0700

    Added license agreement

--- glicko.js
+++ glicko.js
@@ -1,4 +1,6 @@
 //based on http://www.glicko.net/glicko/glicko2.pdf
+//Code by Robert Kohr 2012 (License agreement at the bottom)
+
 
 //wrapper to make it easier to use as a node.js module
 (function(exports){
@@ -327,3 +329,35 @@ returns {
 
 
 })(typeof exports === 'undefined'? this['glicko']={}: exports);//end module
+
+
+/*
+License for this file (and this file only).
+
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+    In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+
+*/
