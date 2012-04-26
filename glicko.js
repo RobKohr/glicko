6319e330b7378de26c620e6a45f568e04ee638fc
Rob Kohr <spam@robkohr.com>
2012-04-26 07:21:18 -0700

    Added glicko module along with an html file for testing

---
+++ glicko.js
@@ -0,0 +1,319 @@
+//based on http://www.glicko.net/glicko/glicko2.pdf
+
+//wrapper to make it easier to use as a node.js module
+(function(exports){
+
+
+    //some helper functions
+    var pow = Math.pow;
+    var pi = Math.PI;
+
+    function ln(val){
+	return Math.log(val) / Math.LOG10E;
+    }
+    var debug = function(v){
+	console.log(JSON.stringify(v));
+    }
+
+    exports.playerInit = function(player){
+	if(!player.rating)
+	    player.rating = 1500;
+	if(!player.rd) //rating deviation
+	    player.rd= 350;
+	if(!player.vol)//volitility
+	    player.vol = 0.06;
+	player.rating2 = (player.rating - 1500)/173.7178
+	player.rd2 = player.rd/173.7178
+	return player;
+    }
+
+    Array.prototype.each = function(callback){
+	var arr = this;
+	for(var i = 0; i<arr.length; i++){
+	    var el = arr[i];
+	    if(typeof(el)!='function')
+		callback(el, i);
+	}
+    }
+
+    Object.prototype.each = function(callback){
+	var obj = this;
+	for(var key in obj){
+	    var el = obj[key];
+	    if(typeof(el)!='function')
+		callback(el, key);
+	}
+    }
+
+
+    /*
+Calculate the adjustments to a player after playing against a set of opponents.
+
+Returns an output of an object like so
+{
+  init:{rd, rating, vol}, //initial values before calculating
+  update:{rd, rating, vol}, //new values
+  change:{rd, rating, vol}, //difference between init and update
+} 
+
+rating_period should be the time in seconds in which a player typically plays
+10-15 games. Default is a month. Currently not used in calculations
+
+For system_constants, the paper says pick a number .3 to 1.2.
+Adjust to achieve better predictive accuracy. Default is .75
+(perhaps a function should be written to calculate a good system_constant)
+
+factor is something I added that isn't in the paper. All outputs are multiplied by
+the factor. This is useful if you want to adjust the value of the calculation -
+for example 0.5 to half the weight when you are playing on a team of two players.
+
+*/
+    exports.calc = function(player, opponents, rating_period, system_constant, factor){
+	//set defaults
+	var min = 60; var hour = 60*min; var day = 24*hour;
+	if(!rating_period)
+	    rating_period = day*30;
+	var t = system_constant;
+	if(!factor)
+	    factor = 1;
+
+	//step 1
+	//initialize
+	if(!t)
+	    t = 0.75;
+
+	//p will be the returned object with the updated values for the player
+	var p = {};
+	p.rating = player.rating;
+	p.rd = player.rd;
+	p.vol = player.vol;
+
+	//initialize all players
+	var initialize_set = [];
+	opponents.each(function(opp){
+	    initialize_set.push(opp);
+	});
+
+	initialize_set.push(p);
+	initialize_set.each(function(pl){
+	    //step 2 - set glicko-2 values (and initial values).
+	    exports.playerInit(pl);
+	});
+
+	var out = {update:{}, change:{}, init:{rating:p.rating, rd:p.rd, vol:p.vol}};
+
+	//step 3
+	var g = function(rd){
+	    return 1/(
+		Math.sqrt(
+		    1 + (
+			(3*pow(rd, 2))/
+			    (pow(pi,2))
+		    )
+		)
+	    );
+	};
+
+	//the paper has three values comming in for the two players, this just takes the player objects
+	var E = function(p1, p2){
+	    if(!p1.rd2)
+		throw "Missing player 1";
+	    if(!p2.rd2)
+		throw "Missing player 2"+p2.rd2;
+	    
+	    return 1 / (1 + Math.exp((-g(p2.rd2)*(p1.rating2-p2.rating2))));
+	}
+
+	//iterate over all opponents he played against to calculate variance
+    	var v_sum = 0;
+	opponents.each(function(opp){
+	    var this_v = (pow(g(opp.rd2),2)) * E(p, opp) * (1 - E(p, opp) );
+	    v_sum += this_v;	    
+	});
+	var v = pow(v_sum, -1);
+
+	//step 4
+	var part_delta_sum = 0;
+	opponents.each(function(opp){
+	    var this_delta_part = g(opp.rd2) * (opp.s - E(p, opp) )
+	    part_delta_sum += this_delta_part;
+	});
+
+	//delta is the change in rating
+	var delta = v * part_delta_sum;
+
+	//step 5
+	//5.1
+	var a = ln(pow(p.vol,2));
+	var f = function(x){
+	    return (
+		(Math.exp(x)*(pow(delta,2)-pow(p.rd2,2)-v-Math.exp(x))) / 
+		    (
+			2* pow( 
+			    (pow(p.rd2,2) + v + Math.exp(x) ),
+			    2)
+		    )
+	    ) - (
+		(x - a)/(pow(t,2))
+	    )
+	}
+	var e = 0.000001;//convergence tolerance
+
+	//5.2
+	var A = a;
+	if( pow(delta,2) > (pow(p.vol,2) + v) ){
+	    var B = ln(pow(delta,2) - pow(p.rd2,2), v);
+	}else{
+	    var k = 1;
+	    while(f(a - (k*Math.sqrt(Math.pow(t,2)))) < 0){
+		k = k+1;
+	    }
+	    var B = a - k * Math.sqrt(Math.pow(t,2));
+	}
+
+	//5.3
+	fa = f(A);
+	fb = f(B);
+
+	//5.4
+	while((Math.abs(B-A) > e)){
+	    var C = A+(A-B)*fa/(fb-fa);
+	    fc = f(C);
+	    if((fc*fb)<0){
+		A = B;
+		fa = fb;
+	    }else{
+		fa = fa/2;
+	    }
+	    B = C;
+	    fb = fc;
+	}
+	var vol_prime = Math.exp(A/2);
+
+	//Step 6
+	var rd2_star = Math.sqrt(Math.pow(p.rd2,2) + Math.pow(vol_prime,2));
+	//Step 7
+	var rd2_prime = 1/(
+	    Math.sqrt( 
+		( (1/(Math.pow(rd2_star, 2))) + 1/v )
+	    ));
+
+	var rating2_prime_sum = 0;
+	opponents.each(function(opp){
+	    var sum_el = g(opp.rd2)*(opp.s - E(p, opp));
+	    rating2_prime_sum += sum_el;
+	});
+	var rating2_prime = p.rating2 + pow(rd2_prime,2) * rating2_prime_sum;
+
+	//lets track changes to player
+	p.rating2 = rating2_prime;
+	p.rd2 = rd2_prime;
+	p.vol = vol_prime;
+
+	//step 8 convert back to original scale
+	p.rating = p.rating2 * 173.7178 + 1500;
+	p.rd = p.rd2 * 173.7171;
+
+	out.change.rating = (p.rating - out.init.rating)*factor;
+	out.change.rd = (p.rd - out.init.rd)*factor;
+	out.change.vol = (p.vol - out.init.vol)*factor;
+
+	out.update.rating = out.change.rating + out.init.rating;
+	out.update.rd = out.change.rd + out.init.rd;
+	out.update.vol = out.change.vol + out.init.vol;	
+	out.timestamp = (new Date()).getTime() / 1000;//in seconds from unix epoch
+	return out;
+    }
+
+
+
+
+/* 
+Example:
+teams = 
+[
+  {
+   rank:1,
+   name:'Blue Jays', 
+   players:
+    [
+      {name:'bob', rating:3300, rd:4},
+      {name:'steve', rating:3300, rd:4},
+    ]
+  },
+  {
+    rank:2,
+    name:"Robins',
+    players:
+    [ ...]
+   }
+];
+
+This function works by calculating what the change would be for each player against all the players
+on the opposing team, averaging them, and then dividing by the number of players on that player's team. 
+
+returns {
+         teams:
+          [same as input, but with all new values applied to players], 
+         players:
+          [array of just updated players (no team structure)]
+        }
+*/
+    exports.teamMatch = function(teams, rating_period, system_constant){
+	var players = [];
+
+	teams.each(function(team, t1){
+	    team.players.each(function(p){
+		players.push(p);
+		var num_players = team.players.length;
+
+		//have the player play against all other teams
+		var opponents = [];
+		//round up opponents
+		teams.each(function(team2, t2){
+		    if(t2==t1)
+			return; //same team as player p
+		    if( (!team2.players) || (!(team2.players.length>0)) )
+			return;//no players on team
+
+
+		    var team_as_player = {rd:0,rating:0,vol:0.06};
+		    team2.players.each(function(p2){
+			exports.playerInit(p2);
+			team_as_player.each(function(val, key){
+			    team_as_player[key] += p2[key];
+			});
+		    });
+		    //team as player has the sum of all players, now 
+		    //need to devide to average it all out.
+		    team_as_player.each(function(val, key){
+			team_as_player[key] = val/team2.players.length;
+		    });
+		    team_as_player.s = exports.s_from_rank(team, team2);
+		    opponents.push(team_as_player);
+		});
+		var result = exports.calc(p, opponents, null, null, 1/num_players);
+		if(!p.result_history) 
+		    p.result_history = [];
+		p.result_history.push(result);
+		p.last_result = result;
+	    });
+	});
+
+	players.each(function(p){
+	    var update = p.last_result.update;
+	    update.each(function(val, key){
+		p[key] = val;
+	    });
+	});
+	return {teams:teams, players:players};
+    }
+    exports.s_from_rank = function(p, p2){
+	if(p2.rank < p.rank)
+	    return 0;
+	if(p2.rank == p.rank)
+	    return .5;
+	return 1;
+    }
+
+})(typeof exports === 'undefined'? this['glicko']={}: exports);//end module
---
+++ tests.js
@@ -0,0 +1,185 @@
+var outClear = function(){
+    document.getElementById('output').innerHTML = ''
+}
+var out = function(obj){
+    document.getElementById('output').innerHTML += '<pre>'+JSON.stringify(obj, null, '\t')+'</pre><hr>';
+}
+
+var basicTest = function(){
+    outClear();
+    var player = {name:'a', rating:1500, rd:200, match_rank:3}
+    var opponents = [//beat b, but lost to c and d
+        {name:'b', rating:1400, rd:30, s:1},
+        {name:'c', rating:1550, rd:100, s:0},
+        {name:'d', rating:1700, rd:300, s:0}
+    ];
+
+    out('player');
+    out(player);
+    out('opponents');
+    out(opponents);
+    var result = glicko.calc(player, opponents, null, null, 1);
+    out('result');
+    out(result);
+    out('Expected result update from paper (vol seems different)');
+    out({
+	rating:1464.06,
+	rd:151.52,
+	vol:0.05999
+    });
+}
+
+var teamTest = function(){
+    var bears = {
+	name:'da bears',
+	players:[
+	    {name:'jim'},
+	    {name:'john'}
+	],
+	rank:1
+    }
+    var vikings = {
+	name:'vikings',
+	players:[
+	    {name:'steve'},
+	    {name:'stan'}
+	],
+	rank:2
+    }
+    outClear();
+    var teams = [bears, vikings];
+    out('Teams');
+    out(teams);
+    var result = glicko.teamMatch(teams);
+    out('Result');
+    out(filterObjectArray(result.players, ['last_result', 'name']));
+}
+
+var randInt = function(min, max){
+    var range = max-min;
+    var offset = Math.floor(Math.random()*range);
+    return offset+min;
+}
+
+var teamSimulate = function(){
+    outClear();
+    out("Description");
+    out("Creates 10 players with skills 1-10. The sum of two players on the team is the team's skill");
+    out("This will run until the ratings of all of the players are ordered the same as their skill");
+    out("Matches are random pairings of 2 vs 2");
+    var players = [];
+    for(var i = 1; i<=10; i++){
+	var player = {name:i+'', expected_rank:i};
+	players.push(player);
+    }
+    var shuffledPlayers = [];
+    players.each(function(player){
+	shuffledPlayers.push(player);
+    })
+
+    for(var c = 1; c<200; c++){
+	//each loop, shuffle up players and deal them into two teams
+	var teams = [];
+	shuffledPlayers.shuffle();
+	sortByProperty(shuffledPlayers, 'rd', 1);
+	var start = randInt(0, players.length-4)
+	var i = 0;
+	for(var t = 0; t<=1; t++){
+	    var team = {rank:0};
+	    team.players = [];
+
+	    for(var p = 0; p<=1; p++){
+		var player = shuffledPlayers[i];
+		i++;
+		team.rank+=player.expected_rank;
+		team.players.push(player);
+	    }
+//	    team.rank = Math.random() * team.rank;
+	    teams.push(team);
+	}
+	var result = glicko.teamMatch(teams);
+	out(filterObjectArray(players, ['expected_rank', 'rating']));
+
+	if(arePlayersProperlyRated(players)){
+	    out('success');
+	    break;//yes they are!
+	}
+	console.log(c);
+    }
+    out('done');
+    out('Number of matches = '+c);
+    
+//    use flot;
+}
+
+var filterObjectArray = function(arr, included_properties_arr){
+    var out = [];
+    arr.each(function(obj){
+	out.push(filterObject(obj, included_properties_arr));
+    });
+    return out;
+}
+					 
+
+var filterObject = function(obj, include){
+    var out = {};
+    include.each(function(key){
+	out[key] = obj[key];
+    });
+    return out;
+}
+
+
+Array.prototype.shuffle = function() {
+    var s = [];
+    while (this.length) s.push(this.splice(Math.random() * this.length, 1)[0]);
+    while (s.length) this.push(s.pop());
+    return this;
+}
+
+
+var randomFromArray = function(arr){
+
+    return arr[i];
+}
+
+//players should already be sorted by skill, so 
+var arePlayersProperlyRated = function(players){
+    sortByProperty(players, 'expected_rank', -1);
+    var prev = null;
+    for(var i=0; i<players.length; i++){
+	var p = players[i];
+	if((prev == null)){
+	    prev = p.rating;
+	    continue
+	}else if(p.rating < prev){
+	    //fine
+	    prev = p.rating;
+	    continue;
+	}else{
+	    if(p.rating==null)
+		console.log('hre', p);
+
+	    out('FAIL -> '+p.rating+' is greater than' +  prev);
+	    return false;//rating should be less than previous
+	}
+    }
+    out('YES!!!');
+    return true;
+}
+
+//order = 1 for asc, -1 for desc
+var sortByProperty = function(arr, prop, order){
+    if(!order)
+	order = 1;
+    arr.sort(function(a, b){
+	if(a[prop] == b[prop])
+	    return 0;
+	if(a.skill > b.skill)
+	    var out = 1;
+	if(a.skill < b.skill)
+	    var out = -1;
+	return order * out;
+    });    
+    return arr;
+}
